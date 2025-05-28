e95203e0f69b47fb242ccd90c50f6dbbe39d9cf5
KaiKimera <git@kai.kim>
2025-05-28 11:18:27 +0300

    Update resources

--- app.hook.sh
+++ app.hook.sh
@@ -11,17 +11,27 @@
 
 (( EUID != 0 )) && { echo >&2 'This script should be run as root!'; exit 1; }
 
+# -------------------------------------------------------------------------------------------------------------------- #
+# CONFIGURATION
+# -------------------------------------------------------------------------------------------------------------------- #
+
+# Sources.
 SRC_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd -P )"
 SRC_NAME="$( basename "$( readlink -f "${BASH_SOURCE[0]}" )" )"
 # shellcheck source=/dev/null
 . "${SRC_DIR}/${SRC_NAME%.*}.conf"
 
+# Parameters.
 DATA=("${DATA:?}"); readonly DATA
 SERVICES=("${SERVICES[@]:?}"); readonly SERVICES
 CRT="${LEGO_CERT_PATH:?}"; readonly CRT
 KEY="${LEGO_CERT_KEY_PATH:?}"; readonly KEY
 PEM="${LEGO_CERT_PEM_PATH:?}"; readonly PEM
 PFX="${LEGO_CERT_PFX_PATH:?}"; readonly PFX
+
+# -------------------------------------------------------------------------------------------------------------------- #
+# -----------------------------------------------------< SCRIPT >----------------------------------------------------- #
+# -------------------------------------------------------------------------------------------------------------------- #
 
 function _if_svc() {
   local service; service="${1}"
